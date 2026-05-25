4f6cb8451458b1193bfe154ac55aea0d0b680475
Luís França <lecfranca.ti@gmail.com>
2026-05-24 22:57:48 -0300

    Add calculations for population density and GDP per capita
    Desafio nível Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,11 +8,11 @@ int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
   char estado[20], codigo_carta[5], cidade[30];
   int populacao, num_pontos_turisticos;
-  float area, pib;
+  float area, pib, densidade_populacional, pib_per_capita;
 
   char estado2[20], codigo_carta2[5], cidade2[30];
   int populacao2, num_pontos_turisticos2;
-  float area2, pib2;
+  float area2, pib2, densidade_populacional2, pib_per_capita2;
 
   // Área para entrada de dados
   printf("Estado: \n");
@@ -36,6 +36,12 @@ int main() {
   printf("Pib: \n");
   scanf("%f", &pib);
 
+  //Calcular a Densidade Populacional: Divida a população da cidade pela sua área
+  densidade_populacional = (float) populacao / area;
+
+  //Calcular o PIB per Capita: Divida o PIB da cidade pela sua população
+  pib_per_capita = (float) pib / populacao;
+
   printf("Estado Nº2: \n");
   scanf("%s", &estado2);
 
@@ -57,10 +63,22 @@ int main() {
   printf("Pib Nº2: \n");
   scanf("%f", &pib2);
 
+  //Calcular a Densidade Populacional: Divida a população da cidade pela sua área
+  densidade_populacional2 = (float) populacao2 / area2;
+
+  //Calcular o PIB per Capita: Divida o PIB da cidade pela sua população
+  pib_per_capita2 = (float) pib2 / populacao2;
+
   // Área para exibição dos dados da cidade
   printf("Estado: %s \nCarta: %s\nPopulação: %s\nQuantidade pontos turísticos: %d\nÁrea: %f\nPib: %f\n", estado, codigo_carta, cidade, populacao, num_pontos_turisticos, area, pib);
+  //Exibir os Resultados Densidade populacional, Pib per Capita:
+  printf("Densidade populacional: %.2f\n", densidade_populacional);
+  printf("Pib per Capita: %.2f\n", pib_per_capita);
 
   printf("Estado: %s \nCarta: %s\nPopulação: %s\nQuantidade pontos turísticos: %d\nÁrea: %f\nPib: %f", estado2, codigo_carta2, cidade2, populacao2, num_pontos_turisticos2, area2, pib2);
-
+  //Exibir os Resultados Densidade populacional, Pib per Capita:
+  printf("Densidade populacional: %.2f\n", densidade_populacional2);
+  printf("Pib per Capita: %.2f\n", pib_per_capita2);
+  
   return 0;
 } 
